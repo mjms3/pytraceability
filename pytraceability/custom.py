890ceb96b27527ea5fe29361cdd78cf5edd024dc
Max Shepherd <shepherd.max@gmail.com>
2025-03-22 16:59:37 +0000

    custom.py: Improve function definition for pytraceability decorator

--- pytraceability/custom.py
+++ pytraceability/custom.py
@@ -2,7 +2,7 @@ from pytraceability.common import traceability
 
 
 class pytraceability(traceability):
-    def __init__(self, key: str, /, info: str):
+    def __init__(self, key: str, *, info: str):
         REQUIRED_PREFIX = "PYTRACEABILITY-"
         if not key.startswith(REQUIRED_PREFIX):
             raise ValueError(f"Key {key} does not start with {REQUIRED_PREFIX}")
--- tests/test_custom.py
+++ tests/test_custom.py
@@ -5,7 +5,7 @@ from pytraceability.custom import pytraceability
 
 @pytest.mark.raises(exception=TypeError)
 def test_custom_decorator_requires_info():
-    @pytraceability("PYTRCAEABILITY-1")  # pyright: ignore
+    @pytraceability("PYTRACEABILITY-1")  # pyright: ignore
     def foo():
         pass
 
@@ -13,5 +13,12 @@ def test_custom_decorator_requires_info():
 @pytest.mark.raises(exception=ValueError)
 def test_custom_decorator_key_must_start_with_PYTRACEABILITY():
     @pytraceability("PYTRCAEABILITY-1", info="info")
+    def foo():
+        pass
+
+
+@pytest.mark.raises(exception=TypeError)
+def test_custom_decorator_info_must_be_a_keyword_argument():
+    @pytraceability("PYTRACEABILITY-1", "info")  # pyright: ignore
     def foo():
         pass

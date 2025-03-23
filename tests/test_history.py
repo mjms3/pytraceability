862255ccc0cb2d183fedec9f9fb7115657b4ae8c
Max Shepherd <shepherd.max@gmail.com>
2025-03-23 10:20:59 +0000

    Test history for adding a new function and decorator

--- tests/test_history.py
+++ tests/test_history.py
@@ -1,13 +1,31 @@
 from dataclasses import replace
+from pathlib import Path
 from unittest import mock
 
 from pytraceability.config import GitHistoryMode
-from tests.examples import function_with_traceability
-from tests.factories import _test_from_module, TEST_CONFIG
+from pytraceability.data_definition import TraceabilityGitHistory
+from pytraceability.discovery import collect_traceability_from_directory
+from tests.factories import TEST_CONFIG
+
+GIT_HISTORY_TESTS_DIR = Path(__file__).parent / "git_history_tests"
+SHOW_HISTORY_CONFIG = replace(
+    TEST_CONFIG, git_history_mode=GitHistoryMode.FUNCTION_HISTORY
+)
 
 
-def test_line_based_git_history():
-    config = replace(TEST_CONFIG, git_history_mode=GitHistoryMode.FUNCTION_HISTORY)
-    _test_from_module(
-        function_with_traceability, config=config, history=[mock.ANY, mock.ANY]
+def test_decorator_added_at_same_time_as_function():
+    actual = list(
+        collect_traceability_from_directory(
+            GIT_HISTORY_TESTS_DIR, GIT_HISTORY_TESTS_DIR, SHOW_HISTORY_CONFIG
+        )
     )
+    assert len(actual) == 1
+    assert actual[0].history == [
+        TraceabilityGitHistory(
+            commit=mock.ANY,
+            author_name=mock.ANY,
+            author_date=mock.ANY,
+            message="History test setup: Add a file with a decorator",
+            diff=mock.ANY,
+        )
+    ]

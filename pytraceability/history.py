3376603d0ebd036701d9a4bad65f4d49a1226034
Max Shepherd <shepherd.max@gmail.com>
2025-03-23 08:12:28 +0000

    Test only: modify function for git history testing

---
+++ pytraceability/history.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class HistoryType(str, Enum):
+    NONE = "none"
+    LINE_BASED = "line-based"
--- tests/examples/function_with_traceability.py
+++ tests/examples/function_with_traceability.py
@@ -3,4 +3,5 @@ from pytraceability.common import traceability
 
 @traceability("A key")
 def foo():
+    print("blah")
     pass

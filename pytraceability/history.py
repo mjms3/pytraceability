2efb9ad523075bc8cf6bfe72fe79ee2ee5c96f81
Max Shepherd <shepherd.max@gmail.com>
2025-03-23 08:13:00 +0000

    Revert "Test only: modify function for git history testing"
    This reverts commit 3376603d0ebd036701d9a4bad65f4d49a1226034.

--- pytraceability/history.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class HistoryType(str, Enum):
-    NONE = "none"
-    LINE_BASED = "line-based"
--- tests/examples/function_with_traceability.py
+++ tests/examples/function_with_traceability.py
@@ -3,5 +3,4 @@ from pytraceability.common import traceability
 
 @traceability("A key")
 def foo():
-    print("blah")
     pass

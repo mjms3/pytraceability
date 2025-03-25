3f2f138407c07bb0f4fea8c05b6aec2f1fefe4df
Max Shepherd <shepherd.max@gmail.com>
2025-03-25 21:49:58 +0000

    History test setup: Move decorator to an entirely different function in an entirely different file

--- tests/git_history_tests/decorator_in_a_new_file.py
+++ tests/git_history_tests/decorator_in_a_new_file.py
@@ -1,6 +1,2 @@
-from pytraceability.common import traceability
-
-
-@traceability("GIT-HISTORY-TEST-4")
 def function_that_moved():
     pass
---
+++ tests/git_history_tests/decorator_moved_to_a_different_function_in_a_new_file.py
@@ -0,0 +1,6 @@
+from pytraceability.common import traceability
+
+
+@traceability("GIT-HISTORY-TEST-4")
+def some_other_function_name():
+    pass
